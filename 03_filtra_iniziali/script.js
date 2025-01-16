ffe33917152731e793699c06c251ba4943767901
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-16 15:14:20 +0100

    esercizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function selezioneLettera(array, lettera) {
+  const restituisce = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][0].toUpperCase() === lettera.toUpperCase()) {
+      restituisce.push(array[i]);
+    }
+  }
+  return restituisce;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+alert(selezioneLettera(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
