7f911acb75bf78cb874012c575fc3aef1cf2bf2d
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-16 15:23:04 +0100

    test perchè non funge

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+function contaVocali(stringa) {
+  const vocali = ["a", "e", "i", "o", "u"];
+  let conteggio = 0;
 
+  for (let i = 0; i < stringa.length; i++) {
+    if (vocali.includes(stringa[i])) {
+      conteggio++;
+    }
+  }
+  return conteggio;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+alert(contaVocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
