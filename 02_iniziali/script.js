8801407880b671754c20512d9a91806fbeb3e9fc
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-16 15:07:37 +0100

    secondo esercizio

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,17 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function iniziali(array) {
+  const iniziale = [];
+  for (let i = 0; i < array.length; i++) {
+    iniziale.push(array[i][0]);
+  }
+  return iniziale;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+alert(iniziali(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
